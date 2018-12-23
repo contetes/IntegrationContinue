1a7096554c1a8d37542e94b73ea658befede616f
contetes <estelle.contet@reseau.eseo.fr>
2018-12-23 22:06:27 +0100

    Change class test

---
+++ TP_IntegrContinue/src/test/java/com/integration_continue/AccueilTest.java
@@ -0,0 +1,19 @@
+package com.integration_continue;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccueilTest {
+
+	@Test
+	public void testAccueil() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testServiceHttpServletRequestHttpServletResponse() {
+		assertTrue(true);
+	}
+
+}
